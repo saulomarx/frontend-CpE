cfae3bd7aaa5be4cf2cb5cf90afc20cea2f178fe
Luiz Augusto <luiz.moraes@drtis.com.br>
2019-04-11 01:14:13 -0300

    add initial setup to frontend project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Start of the application
+      </div>
+    );
+  }
+}
+
+export default App;
